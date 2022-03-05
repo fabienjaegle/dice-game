3e62f0e66aef66bf6f2774a0c1a4bcadd4189d7e
Fabien Jaegle <fab6887@gmail.com>
2022-03-06 00:06:58 +0100

    Add comments

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var scores, currentPlayer, currentPlayerIndex, roundScore, isPlaying;
 init();
 
 function init() {
+    // initialize the game
     scores = [0, 0];
     players = [PLAYER_1, PLAYER_2];
     currentPlayer = 1;
@@ -14,6 +15,7 @@ function init() {
     roundScore = 0;
     isPlaying = true;
 
+    // update the UI
     document.getElementById('dice-xs').style.display = 'none';
     document.getElementById('dice-sm').style.display = 'none';
     document.getElementById('score-1').textContent = '0';
@@ -46,24 +48,28 @@ function rollDice() {
     if (isPlaying) {
         var dice = Math.floor(Math.random() * 6) + 1;
 
+        // update the UI
         document.getElementById('lost-round-xs').style.display = 'none';
         document.getElementById('lost-round-sm').style.display = 'none';
 
+        // add correct dice image
         document.getElementById('dice-xs').style.display = 'inline-block';
         document.getElementById('dice-xs').src = 'img/dice-' + dice + '.png';
         document.getElementById('dice-sm').style.display = 'inline-block';
         document.getElementById('dice-sm').src = 'img/dice-' + dice + '.png';
 
         if (dice !== 1) {
-            //add score
+            // add score
             roundScore += dice;
             document.querySelector('#current-' + currentPlayer).textContent = roundScore;
         } else {
+            // player lost the round
             document.getElementById('current-player-xs').textContent = players[currentPlayerIndex];
             document.getElementById('current-player-sm').textContent = players[currentPlayerIndex];
             document.getElementById('lost-round-xs').style.display = 'block';
             document.getElementById('lost-round-sm').style.display = 'block';
             
+            // let playing the next player
             nextPlayer();
         }
     }
@@ -74,6 +80,7 @@ function nextPlayer() {
     currentPlayerIndex === 0 ? currentPlayerIndex = 1 : currentPlayerIndex = 0;
     roundScore = 0;
 
+    // update the UI
     document.getElementById('current-1').textContent = '0';
     document.getElementById('current-2').textContent = '0';
 
@@ -89,10 +96,10 @@ function nextPlayer() {
 
 function hold() {
     if (isPlaying) {
-        //add current score to the global score
+        // add current score to the global score
         scores[currentPlayerIndex] += roundScore;
 
-        //Update the UI
+        // update the UI
         document.querySelector('#score-' + currentPlayer).textContent = scores[currentPlayerIndex];
 
         // check if player win the game
@@ -104,6 +111,7 @@ function hold() {
             document.getElementById('game-buttons').style.display = 'none';
             isPlaying = false;
         } else {
+            // let playing the next player
             nextPlayer();
         }
     }
